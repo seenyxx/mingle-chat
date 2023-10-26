8b8e6094a7138b3be18954be7592c7b920e2d09c
seenyxx <seenyxx@github.com>
2023-10-26 13:23:55 +1100

    partially created the friends service firebase interaction

---
+++ lib/services/accounts/friends_serivce.dart
@@ -0,0 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FriendsService extends ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getFriendData(String uid) async {
+    return await _firestore.collection('friends').doc(uid).get();
+  }
+
+  Future<List<String>> getFriendsList([String? uid]) async {
+    Map<String, dynamic>? friendData =
+        (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
+
+    if (friendData == null) {
+      return [];
+    }
+
+    return friendData.toString().contains('friends') ? friendData['friends'] : [];
+  }
+
+  Future<void> addFriend(String otherUserUid) async {
+    String currentUserUid = _firebaseAuth.currentUser!.uid;
+    List<String> currentFriendsList = await getFriendsList();
+    List<String> currentFriendRequests = await getFriendRequests();
+    List<String> otherUserFriendsList = await getFriendsList(otherUserUid);
+
+    if (!currentFriendsList.contains(otherUserUid)) {
+      currentFriendsList.add(otherUserUid);
+    }
+
+    if (!otherUserFriendsList.contains(currentUserUid)) {
+      otherUserFriendsList.add(currentUserUid);
+    }
+
+    if (currentFriendRequests.contains(otherUserUid)) {
+      currentFriendRequests.remove(otherUserUid);
+    }
+
+    // Update current user's friend requests list
+    await _firestore
+        .collection('friends')
+        .doc(currentUserUid)
+        .set({'friendRequests': currentFriendRequests}, SetOptions(merge: true));
+
+    // Update current user friends list
+    await _firestore
+        .collection('friends')
+        .doc(currentUserUid)
+        .set({'friends': currentFriendsList}, SetOptions(merge: true));
+
+    // Update other user's friends list
+    await _firestore
+        .collection('friends')
+        .doc(otherUserUid)
+        .set({'friends': otherUserFriendsList}, SetOptions(merge: true));
+  }
+
+  Future<List<String>> getFriendRequests([String? uid]) async {
+    Map<String, dynamic>? friendData =
+        (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
+
+    if (friendData == null) {
+      return [];
+    }
+
+    return friendData.toString().contains('friendRequests')
+        ? friendData['friendRequests']
+        : [];
+  }
+
+  Future<List<String>> getActiveChatRooms([String? uid]) async {
+    Map<String, dynamic>? friendData =
+        (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
+
+    if (friendData == null) {
+      return [];
+    }
+
+    return friendData.toString().contains('chatRooms') ? friendData['chatRooms'] : [];
+  }
+}
