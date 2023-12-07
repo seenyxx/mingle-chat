f4030d3f9d0f434f98ab9dfa5a16bd36a7017ee6
aRandomGithubUser <61892371+aRandomGithubUser@users.noreply.github.com>
2023-12-07 23:13:01 +1100

    did some stuff ig

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:focused_menu/modals.dart';
+import 'package:minglechat/components/avatar_skeleton.dart';
 import 'package:minglechat/components/dm_skeleton.dart';
+import 'package:minglechat/services/accounts/friends_serivce.dart';
+import 'package:minglechat/services/accounts/profile_service.dart';
 
 class FriendsPage extends StatefulWidget {
   const FriendsPage({super.key});
@@ -9,19 +14,135 @@ class FriendsPage extends StatefulWidget {
 }
 
 class _FriendsPageState extends State<FriendsPage> {
+  final FriendsService _friendsService = FriendsService();
+  final ProfileService _profileService = ProfileService();
+  static const Color _skeletonColor = Color(0xFF21212F);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Friends'),
-        leading: IconButton(
-          highlightColor: Colors.transparent,
-          splashRadius: 20,
-          onPressed: () => Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Friends'),
+          leading: IconButton(
+            highlightColor: Colors.transparent,
+            splashRadius: 20,
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          ),
+          bottom: const TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.people_alt_outlined),
+              text: 'Friends',
+            ),
+            Tab(
+              icon: Icon(Icons.group_add),
+              text: 'Friend Requests',
+            )
+          ]),
         ),
+        body: TabBarView(children: <Widget>[
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: _buildFriendList(),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Container(),
+          ),
+        ]),
       ),
-      body: const Column(children: [DirectMessagesSkeleton()]),
     );
+  }
+
+  Widget _buildFriendList() {
+    return FutureBuilder<List<String>>(
+        future: _friendsService.getFriendsList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.separated(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) => _buildFriendListItem(snapshot.data![index]),
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+          );
+        });
+  }
+
+  Widget _buildFriendListItem(String uid) {
+    return FutureBuilder(
+        future: _profileService.getUserProfile(uid),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const DirectMessagesSkeleton();
+          }
+
+          Map<String, dynamic> profile = snapshot.data!.data() as Map<String, dynamic>;
+
+          return ListTile(
+            contentPadding: const EdgeInsets.all(15),
+            leading: FutureBuilder(
+                future: _profileService.getProfileAvatarUrl(profile['uid']),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const AvatarSkeleton(radius: 20);
+                  }
+
+                  if (snapshot.data == null) {
+                    return const AvatarSkeleton(radius: 20);
+                  }
+
+                  return CircleAvatar(
+                    radius: 20,
+                    backgroundColor: _skeletonColor,
+                    backgroundImage: NetworkImage(snapshot.data!),
+                  );
+                }),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(profile['displayName']),
+                Text(
+                  '@${profile['username']}',
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                )
+              ],
+            ),
+            trailing: MenuAnchor(
+              builder: (context, controller, child) {
+                return IconButton(
+                  color: Colors.white,
+                  splashRadius: 15,
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () {
+                    if (controller.isOpen) {
+                      controller.close();
+                    } else {
+                      controller.open();
+                    }
+                  },
+                );
+              },
+              menuChildren: [const MenuItemButton(child: Text('a'))],
+            ),
+          );
+        });
   }
 }
--- lib/services/accounts/friends_serivce.dart
+++ lib/services/accounts/friends_serivce.dart
@@ -6,19 +6,21 @@ class FriendsService extends ChangeNotifier {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<DocumentSnapshot<Map<String, dynamic>>> getFriendData(String uid) async {
-    return await _firestore.collection('friends').doc(uid).get();
+  Future<DocumentSnapshot<Map<String, dynamic>>> getFriendData(String uid) {
+    return _firestore.collection('friends').doc(uid).get();
   }
 
   Future<List<String>> getFriendsList([String? uid]) async {
-    Map<String, dynamic>? friendData =
-        (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
+    var friendData = (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
 
     if (friendData == null) {
       return [];
     }
 
-    return friendData.toString().contains('friends') ? friendData['friends'] : [];
+    return friendData.toString().contains('friends')
+        ? List<String>.from(friendData['friends'])
+        : [];
+    // return friendData;
   }
 
   Future<void> addFriend(String otherUserUid) async {
@@ -71,14 +73,37 @@ class FriendsService extends ChangeNotifier {
         : [];
   }
 
-  Future<List<String>> getActiveChatRooms([String? uid]) async {
-    Map<String, dynamic>? friendData =
-        (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
-
-    if (friendData == null) {
-      return [];
+  Future<void> removeFriendRequest(String otherUserUid, [String? uid]) async {
+    List<String> currentFriendRequests =
+        await getFriendRequests(uid ?? _firebaseAuth.currentUser!.uid);
+    if (currentFriendRequests.contains(otherUserUid)) {
+      currentFriendRequests.remove(otherUserUid);
     }
-
-    return friendData.toString().contains('chatRooms') ? friendData['chatRooms'] : [];
   }
+
+  Future<void> addFriendRequest(String otherUserUid, [String? uid]) async {
+    List<String> currentFriendRequests =
+        await getFriendRequests(uid ?? _firebaseAuth.currentUser!.uid);
+    if (!currentFriendRequests.contains(otherUserUid)) {
+      currentFriendRequests.add(otherUserUid);
+    }
+  }
+
+  // Future<List<String>> getActiveChatRooms([String? uid]) async {
+  //   Map<String, dynamic>? friendData =
+  //       (await getFriendData(uid ?? _firebaseAuth.currentUser!.uid)).data();
+
+  //   if (friendData == null) {
+  //     return [];
+  //   }
+
+  //   return friendData.toString().contains('chatRooms') ? friendData['chatRooms'] : [];
+  // }
+
+  // Future<List<String>> addActiveChatRoom(String otherUserUid, [String? uid]) async {
+  //   List<String> chatrooms =
+  //       await getActiveChatRooms(uid ?? _firebaseAuth.currentUser!.uid);
+  //   List<String> otherUserChatrooms = await getActiveChatRooms(otherUserUid);
+
+  // }
 }
